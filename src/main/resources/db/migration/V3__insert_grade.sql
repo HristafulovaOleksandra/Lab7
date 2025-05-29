18df9a19ea4188edd74ae40c22b90ceeb61de50c
HristafulovaOleksandra <hristafulovaoleksandra@gmail.com>
2025-05-29 17:59:03 +0300

    V3_insert_gradeSuccess

---
+++ src/main/resources/db/migration/V3__insert_grade.sql
@@ -0,0 +1,6 @@
+INSERT INTO grade (subject, score, student_id) VALUES
+            ('Math', 95, 1),
+            ('Patterns', 88, 1),
+            ('DataBase', 76, 2),
+            ('English', 90, 3),
+            ('Patterns', 85, 3);
