db5c76cd951690e8d2a3e9edfa38e95ecfe79504
HristafulovaOleksandra <hristafulovaoleksandra@gmail.com>
2025-05-29 17:49:33 +0300

    V2_insert_studentSuccess

---
+++ src/main/resources/db/migration/V2__insert_student.sql
@@ -0,0 +1,4 @@
+INSERT INTO student (name, email) VALUES
+            ('Oleksandra Hristafulova', 'hristafulova@example.com'),
+            ('Fiona Kovalenko', 'fiona.2000@gmail.com'),
+            ('Roman Frolov', 'Frolovbusiness@outlook');
